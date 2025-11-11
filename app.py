5bdb19de4a38c688dfc8f2e88ff348065554adfa
Mario-bro777 <mariobro777ai@gmail.com>
2025-11-12 00:43:42 +0500

    json endpoint added

--- app.py
+++ app.py
@@ -10,3 +10,10 @@ def index():
 def search():
     quest = request.args.get("q")
     return redirect(f"https://www.google.com/search?q={quest}")
+
+@app.get("/json")
+def json():
+    arg = request.args.get("name")
+    if not arg:
+        return jsonify({"status":"error", "data":"wrong parameter"})
+    return jsonify({"status":"success", "data":f"your request is {arg}"})
